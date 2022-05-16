0c09e3b994287f71dee0f794efed52669dfa8ec1
yuya kinjo <22kinjo_yuuya@axiz.co.jp>
2022-05-16 12:17:40 +0900

    a

--- src/main/java/app/StartAppServlet.java
+++ src/main/java/app/StartAppServlet.java
@@ -63,11 +63,17 @@ public class StartAppServlet extends HttpServlet {
       //入力値取得
 	  ((HttpServletRequest) request).setCharacterEncoding("UTF-8");             //appStartの文字コードを取得する
       String name = request.getParameter("name");        //nameをリクエストし、新しくname変数に代入
-
+      String g = request.getParameter("g");
      if (name != null && !name.isEmpty()) {
       	//GameAppクラスのオブジェクト
     	//引数つきコンストラクタで、itemフィールドに"何か"をセット）
-    	  GameApp app = new GameApp("何か");
+    	 GameApp app;   //appをif文の外で宣言
+    	 if(g.equals("trump")){
+       	  app = new CardGameApp("トランプ");
+    	 }else {
+    	  app = new GameApp("何か");
+    	 }
+ 
     	  
 		//GameAppオブジェクトのstartメソッドを呼ぶ
     	  String result = app.start(name);                    //appフォルダにあるstartのファイル
